e84df420775393e0ea3b007b6b072c69e7c96363
ttrichar <trevor.richardson@fathym.com>
2019-02-01 14:27:06 -0700

    Add loading

--- projects/api/src/lib/observable-context/observable-context.service.ts
+++ projects/api/src/lib/observable-context/observable-context.service.ts
@@ -2,19 +2,31 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 export abstract class ObservableContextService<T> {
   //	Fields
+  protected loader: BehaviorSubject<boolean>;
+
   protected subject: BehaviorSubject<T>;
 
   //	Properties
   public readonly Context: Observable<T>;
 
+  public readonly Loading: Observable<boolean>
+
   //  Constructors
   constructor() {
+    this.loader = new BehaviorSubject<boolean>(false);
+
     this.subject = new BehaviorSubject<T>(this.defaultValue());
 
-	  this.Context = this.subject.asObservable();
+    this.Context = this.subject.asObservable();
+    
+    this.Loading = this.loader.asObservable();
   }
 
-	//  Helpers
+  //  Helpers 
+  protected loading(loading: boolean) {
+    this.loader.next(loading);
+  }
+
 	protected defaultValue(): T {
 		return null;
 	}
